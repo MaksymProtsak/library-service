afe5fe1c7ef9e4e59eee5f309a4818f47bcfe0ae
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-12-17 08:46:19 +0200

    Created BookSerializer.

---
+++ books_app/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from books_app.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "cover", "daily_fee", "inventory")
