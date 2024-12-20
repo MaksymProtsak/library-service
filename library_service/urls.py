7e005bcdfbf4d87fb2964cd214990b063f8af1b1
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-20 07:47:34 +0200

    Added borrowings_app in urlpatterns.

--- library_service/urls.py
+++ library_service/urls.py
@@ -27,8 +27,9 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/", include("books_app.urls", namespace="books")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/books_app/", include("books_app.urls", namespace="books_app")),
+    path("api/user/", include("user.urls", namespace="user_app")),
+    path("api/borrowings_app/", include("borrowings_app.urls", namespace="borrowings_app")),
     path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
