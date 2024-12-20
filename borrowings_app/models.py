d0f5821c71f21542d3bff1d8d2425e59a9cefa94
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-12-20 07:01:21 +0200

    Created Borrowing model.

--- borrowings_app/models.py
+++ borrowings_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    book_id = models.IntegerField()
+    user_id = models.IntegerField()
