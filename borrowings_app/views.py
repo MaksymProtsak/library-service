e9122f8120cee79a6454fd0ae228efbd1796d40c
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-12-20 07:21:18 +0200

    Created BorrowViewSet.

--- borrowings_app/views.py
+++ borrowings_app/views.py
@@ -1,3 +1,11 @@
+from rest_framework import viewsets
+
 from django.shortcuts import render
 
-# Create your views here.
+from borrowings_app.models import Borrowing
+from borrowings_app.serializers import BorrowSerializer
+
+
+class BorrowViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowSerializer
