5544c8c861066eb3d6411850b18e3698293fd7e3
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-20 13:58:27 +0200

    Renamed book_id to book, user_id to user in BorrowSerializer.Meta. Created PartialUserSerializer.

--- borrowings_app/serializers.py
+++ borrowings_app/serializers.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from books_app.serializers import BookSerializer
 from borrowings_app.models import Borrowing
+from user.serializers import UserSerializer
 
 
 class BorrowSerializer(serializers.ModelSerializer):
@@ -10,10 +13,17 @@ class BorrowSerializer(serializers.ModelSerializer):
             "id",
             "borrow_date",
             "expected_return_date",
-            "book_id",
-            "user_id",
+            "book",
+            "user",
         )
 
 
+class PartialUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "email")
+
+
 class ReadBorrowSerializer(BorrowSerializer):
-    ...
+    book = BookSerializer(read_only=True)
+    user = PartialUserSerializer(read_only=True,)
