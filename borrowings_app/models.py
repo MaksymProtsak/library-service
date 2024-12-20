3887ae13f50fda107e5505c226607008f29b2242
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-20 13:56:48 +0200

    Renamed book_id to book, added foreign key link. Renamed user_id to user, added foreign key link.

--- borrowings_app/models.py
+++ borrowings_app/models.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.conf import settings
+
+from books_app.models import Book
 
 
 class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
-    book_id = models.IntegerField()
-    user_id = models.IntegerField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
