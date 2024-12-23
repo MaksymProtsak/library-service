4668e2cc200e800b89e99becfbafc2aeee635508
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-12-23 08:19:46 +0200

    Updated get_serializer_class method with action condition.

--- books_app/views.py
+++ books_app/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
 
-from django.shortcuts import render
-
 from books_app.models import Book
-from books_app.serializers import BookSerializer
+from books_app.serializers import BookSerializer, BookListSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        return self.serializer_class
