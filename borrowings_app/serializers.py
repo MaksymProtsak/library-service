0664d69196663c47acc918b725bbf0580fdd1ae9
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-23 13:35:20 +0200

    Created 'create' method in BorrowSerializer.

--- borrowings_app/serializers.py
+++ borrowings_app/serializers.py
@@ -1,9 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from books_app.models import Book
 from books_app.serializers import BookSerializer
 from borrowings_app.models import Borrowing
-from user.serializers import UserSerializer
 
 
 class BorrowSerializer(serializers.ModelSerializer):
@@ -15,6 +15,14 @@ class BorrowSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "book",
         )
+
+    def create(self, validated_data):
+        count = Book.objects.filter(title=validated_data["book"].title).count()
+        book_inventory = Book.objects.filter(title=validated_data["book"].inventory)
+        if book_inventory:
+            Book.objects.filter(title=validated_data["book"].title).update(inventory=count - 1)
+            return Borrowing.objects.create(**validated_data)
+        raise ValueError("Book not found") # Need to use validate method from Borrowing model
 
 
 class PartialUserSerializer(serializers.ModelSerializer):
