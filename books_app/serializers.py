dbb756794954d88953e587577b6a9e77c1d52212
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-12-23 08:18:52 +0200

    Created BookListSerializer.

--- books_app/serializers.py
+++ books_app/serializers.py
@@ -6,4 +6,11 @@ from books_app.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ("id", "title", "author", "cover", "daily_fee", "inventory")
+        fields = ("id", "title", "author", "cover", "daily_fee",)
+
+
+class BookListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "cover", "daily_fee", "inventory",)
+
