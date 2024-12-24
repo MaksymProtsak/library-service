91d57c44c02eb52f822de3a082aa974743d6c90a
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-12-24 08:19:49 +0200

    Inherited DestroyModelMixin class. Override perform_destroy method.

--- borrowings_app/views.py
+++ borrowings_app/views.py
@@ -1,14 +1,19 @@
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
+from books_app.models import Book
 from borrowings_app.models import Borrowing
-from borrowings_app.serializers import BorrowSerializer, ReadBorrowSerializer
+from borrowings_app.serializers import (
+    BorrowSerializer,
+    ReadBorrowSerializer
+)
 
 
 class BorrowViewSet(
     mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
     mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
     GenericViewSet,
 ):
     queryset = Borrowing.objects.all()
@@ -21,3 +26,8 @@ class BorrowViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        book = instance.book
+        count = Book.objects.filter(title=book.title).count()
+        Book.objects.filter(title=book.title).update(inventory=count)
