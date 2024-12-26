a7491d7b1c112f5a0b8687218df9ece9595e30cd
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-12-26 08:30:11 +0200

    Removed DestroyModelMixin from inheritance of BorrowingViewSet. Removed perform_destroy method. Created elif condition in get_serializer_class with action borrowing_return. Created borrowing_return method.

--- borrowings_app/views.py
+++ borrowings_app/views.py
@@ -2,13 +2,13 @@ from rest_framework import mixins, status
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 
-from books_app.models import Book
 from borrowings_app.models import Borrowing
 from borrowings_app.serializers import (
     BorrowingSerializer,
     ReadBorrowingSerializer,
-    BorrowingListSerializer,
+    BorrowingListSerializer, BorrowingReturnSerializer,
 )
 
 
@@ -16,7 +16,6 @@ class BorrowingViewSet(
     mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
     mixins.ListModelMixin,
-    mixins.DestroyModelMixin,
     GenericViewSet,
 ):
     queryset = Borrowing.objects.all()
@@ -28,20 +27,36 @@ class BorrowingViewSet(
             return ReadBorrowingSerializer
         elif self.action == "list":
             return BorrowingListSerializer
+        elif self.action == "borrowing_return":
+            return BorrowingReturnSerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-    def perform_destroy(self, instance):
-        book = instance.book
-        count = Book.objects.filter(title=book.title).count()
-        Book.objects.filter(title=book.title).update(inventory=count)
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_queryset(self):
         if self.request.user.is_staff:
             return Borrowing.objects.all()
 
         return Borrowing.objects.filter(user=self.request.user)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="return",
+        permission_classes=[IsAuthenticated],
+    )
+    def borrowing_return(self, request, pk=None):
+        """
+        Endpoint for return book.
+        For borrowing instance, updated filed actual_return_date.
+        For book, inventory for all books with same title rose +1.
+        """
+        borrowing = self.get_object()
+        serializer = self.get_serializer(borrowing, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
