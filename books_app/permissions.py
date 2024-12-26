8e074f0493d347683830a829a5e2eb3104f339de
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-26 06:56:57 +0200

    Renamed IsAdminOrIfAuthenticatedReadOnly to IsAdminOrAnyUserReadOnly.

--- books_app/permissions.py
+++ books_app/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+class IsAdminOrAnyUserReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.method in SAFE_METHODS
