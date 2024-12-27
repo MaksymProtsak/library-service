c39a290066d576d829dc16331a841b072a7ebca5
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-27 14:00:23 +0200

    Created test_create_book_forbidden in AuthenticatedBookApiTests. Created class AdminBookTests with metho test_create_book.

--- books_app/tests.py
+++ books_app/tests.py
@@ -8,16 +8,17 @@ from rest_framework import status
 from books_app.models import Book
 from books_app.serializers import BookListSerializer
 
+from decimal import Decimal
+
 BOOK_URL = reverse("books_app:book-list")
 
 
 def sample_airplane(**params) -> Book:
     defaults = {
-            "title": "HHarry Potter and the Philosopher's Stone",
-            "author": "Joanne Rowling",
-            "cover": "SOFT",
-            "daily_fee": "1",
-            "inventory": "5",
+        "title": "Harry Potter and the Philosopher's Stone",
+        "author": "Joanne Rowling",
+        "cover": "SOFT",
+        "daily_fee": "1",
     }
     defaults.update(params)
     return Book.objects.create(**defaults)
@@ -48,3 +49,56 @@ class AuthenticatedBookApiTests(TestCase):
         res = self.client.get(BOOK_URL)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_create_book_forbidden(self):
+        payload = {
+            "title": "Harry Potter and the Philosopher's Stone",
+            "author": "Joanne Rowling",
+            "cover": "Soft",
+            "daily_fee": "1",
+        }
+        res = self.client.post(path=BOOK_URL, data=payload)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminBookTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="admin@admin.com",
+            password="testpassword",
+            is_staff=True,
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_create_book(self):
+        book_inventory = 1
+        payload = {
+            "title": "Harry Potter and the Philosopher's Stone",
+            "author": "Joanne Rowling",
+            "cover": "SOFT",
+            "daily_fee": "2",
+        }
+        res = self.client.post(path=BOOK_URL, data=payload)
+        book = Book.objects.get(id=res.data["id"])
+        book_title = payload["title"]
+        book_author = payload["author"]
+        book_cover = payload["cover"].lower().capitalize()
+        book_daily_fee = Decimal(
+            payload['daily_fee']
+        ).quantize(Decimal('1.00'))
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(book.title, payload["title"])
+        self.assertEqual(book.author, payload["author"])
+        self.assertEqual(book.cover, payload["cover"])
+        self.assertEqual(book.daily_fee, Decimal(payload["daily_fee"]))
+        self.assertEqual(book.inventory, book_inventory)
+        self.assertEqual(
+            str(book),
+            f"{book_title} "
+            f"by {book_author} "
+            f"({book_cover}) - "
+            f"${book_daily_fee}/day "
+            f"(in stock: {book_inventory})"
+        )
