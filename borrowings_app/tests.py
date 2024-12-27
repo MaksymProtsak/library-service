a5142cb4951a6e6d39ed86f6990129c5ce8a2c54
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-27 14:06:38 +0200

    Created UnauthenticatedAirplaneApiTests class with method test_auth_required.

--- borrowings_app/tests.py
+++ borrowings_app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework.reverse import reverse
+from rest_framework import status
+
+AIRPLANE_URL = reverse("borrowings_app:borrowing-list")
+
+
+class UnauthenticatedAirplaneApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(AIRPLANE_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
