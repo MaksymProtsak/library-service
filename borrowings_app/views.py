81eff50bd3d95984759ec491a619db3dbaee99f6
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-29 12:42:40 +0200

    Created documentation for create, list, retrieve methods.

--- borrowings_app/views.py
+++ borrowings_app/views.py
@@ -4,6 +4,12 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiParameter,
+    OpenApiExample
+)
+
 from borrowings_app.models import Borrowing
 from borrowings_app.serializers import (
     BorrowingSerializer,
@@ -30,6 +36,10 @@ class BorrowingViewSet(
         elif self.action == "borrowing_return":
             return BorrowingReturnSerializer
         return self.serializer_class
+
+    def create(self, request, *args, **kwargs):
+        """Created a new borrowing"""
+        return super().create(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -68,3 +78,65 @@ class BorrowingViewSet(
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "is_active",
+                type=str,
+                description="Filter by Null actual_return_date param",
+                required=False,
+                examples=[
+                    OpenApiExample(
+                        name="Filter by Null actual_return_date",
+                        value="True",
+                        description="Filter by Null actual_return_date param."
+                    )
+                ]
+            ),
+            OpenApiParameter(
+                "is_active",
+                type=str,
+                description="Filter by Null actual_return_date param",
+                required=True,
+                examples=[
+                    OpenApiExample(
+                        name="Filter by Null actual_return_date",
+                        value="True",
+                        description="Show all borrowing books"
+                    )
+                ]
+            ),
+            OpenApiParameter(
+                "user_id",
+                type=str,
+                description="Filter by user id",
+                required=False,
+                examples=[
+                    OpenApiExample(
+                        name="Filter by user id",
+                        value="1",
+                        description=(
+                                "Filter borrowing by user id. "
+                                "If you are admin you can filter all borrowings by user"
+                        )
+                    ),
+                    OpenApiExample(
+                        name="Filter by user id",
+                        value="2",
+                        description=(
+                                "Filter borrowing by user id. "
+                                "If you are admin you can filter all borrowings by user"
+                        )
+                    ),
+                ]
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        """Retrieve a list of borrowing"""
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        """Retrieve a single borrowing"""
+        return super().retrieve(request, *args, **kwargs)
