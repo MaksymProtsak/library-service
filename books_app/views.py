11d0f5062374ed650f2d7281236284d5cd1fddd4
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-29 12:21:21 +0200

    Added documentation for list, create, retrieve, destroy methods.

--- books_app/views.py
+++ books_app/views.py
@@ -12,3 +12,19 @@ class BookViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             return BookListSerializer
         return self.serializer_class
+
+    def list(self, request, *args, **kwargs):
+        """Retrieve list of books"""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create a new book"""
+        return super().create(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        """Retrieve a specific book"""
+        return super().retrieve(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        """Remove a specific book"""
+        return super().destroy(request, args, kwargs)
