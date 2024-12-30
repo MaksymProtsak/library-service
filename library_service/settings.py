2b8050a8c5945356d32167f1c23d6a7fb57d262f
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-30 12:33:48 +0200

    Set SECRET_KEY from .env.Set BEGUG from .env. Added localhost to ALLOWED_HOSTS.

--- library_service/settings.py
+++ library_service/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 
 from datetime import timedelta
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,13 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
-    "django-insecure-i12gix^#xfky6(fu7h7^)=+rkcw3z6!4vn-#0-p!qgp3&ti8xy"
+    os.environ.get("DJANGO_SECRET_KEY", "default not secure key")
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",]
 
 # Application definition
 
